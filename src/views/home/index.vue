3c2a62c9319f154cc214f70fa6abc387dd1db6d4
InChh <2319397152@outlook.com>
2024-10-08 19:21:19 +0800

    feat(projects): Delete unused component on home page

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -8,7 +8,6 @@ import StatisticTable from '@/views/_common/statistic-table.vue';
   <NSpace vertical :size="16">
     <HeaderBanner />
     <CardData />
-    <NCard>计划在此处整合李磊开发的排班系统</NCard>
     <NCard :title="$t('page.statistic.title')">
       <StatisticTable />
     </NCard>
